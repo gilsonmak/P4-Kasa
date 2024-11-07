837a6a0791c2d405939814f7afd9eaa637bf35c8
gilsonmak <gilsonmakanounou@yahoo.com>
2024-11-08 00:01:11 +0100

    feat: intégration de la banner sur la page About

--- src/Pages/AboutPage/AboutPage.jsx
+++ src/Pages/AboutPage/AboutPage.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import Banner from "../../Components/Banner/Banner";
+import BackgroundAbout from "../../Assets/banner/BannerAbout.png"
+import "./AboutPage.scss"
 
 
 
 function AboutPage() {
 
     return (
-        <div>toto</div>
+
+        <div>
+
+             <Banner bannerImage={BackgroundAbout} alt="Banner About" />
+
+
+
+
+        </div>
     )
 }
 export default AboutPage
