eca043f1478b54a66b1b381dbf035938f593eb76
gilsonmak <gilsonmakanounou@yahoo.com>
2024-11-12 13:13:52 +0100

    refactor: simplification du code

--- src/Components/Collapse/Collapse.jsx
+++ src/Components/Collapse/Collapse.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import arrow from "../../Assets/arrow/down.png"
 import "./Collapse.scss"
 
@@ -7,11 +7,19 @@ import "./Collapse.scss"
 function Collapse ({title, description}) {
 
     const [open, setOpen] = useState(false);
+    const [ height, setHeight] = useState(0)
+    const ContentRef = useRef (null);
 
 
     const toggleCollapse = () => {
         setOpen(!open);
     };
+
+
+    useEffect(() => {
+        if (open) setHeight(ContentRef.current?.scrollHeight);
+        else setHeight(0);
+      }, [open]);
 
     return (
         <div className={`collapse-container ${open ? "open" : ""}`}>
@@ -20,7 +28,7 @@ function Collapse ({title, description}) {
                 <img src={arrow} alt='flèche' className={`arrow-icon ${open ? "arrow-down" : "arrow-up"}`} />
             </div>
 
-            <div className={`collapse-description ${open ? "open" : ""}`}> 
+            <div ref={ContentRef} className="collapse-description" style={{ maxHeight: `${height}px` }}> 
                 {description}
             </div>
         </div>
